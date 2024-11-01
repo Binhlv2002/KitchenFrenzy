3929f0800941109e8f1346bd37d2dd44abd7f223
Binhlv2002 <104196786+Binhlv2002@users.noreply.github.com>
2024-11-01 13:49:51 +0700

    edit option menu

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,15 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+    [SerializeField] private Button playAgainButton;
+    [SerializeField] private Button exitToMenuButton;
 
     private void Start()
     {
         GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
+        playAgainButton.onClick.AddListener(RestartGame);
+        exitToMenuButton.onClick.AddListener(ExitToMenu);
         Hide();
     }
 
@@ -36,5 +42,15 @@ public class GameOverUI : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-   
+    private void RestartGame()
+    {
+        // Tải lại cảnh hiện tại để bắt đầu lại trò chơi
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void ExitToMenu()
+    {
+        // Tải cảnh menu chính (MainMenuScene) 
+        SceneManager.LoadScene("MainMenuScene");
+    }
 }
